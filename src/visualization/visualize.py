af6c1911d1275a85ad7bf5b9465372105575c22c
pq.lz.alejandro@gmail.com <pq.lz.alejandro@gmail.com>
2024-05-20 18:32:39 +0000

    VISUALIZATION FOLDER DONE and TESTED

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -8,6 +8,7 @@ Functionality: This file contains the code to plot the data of the project.
 # Imports
 # =====================================================================
 import random
+from typing import List, Dict, Union
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
@@ -26,13 +27,17 @@ poke_df = pd.read_csv(f"{DATA_PATH}/raw/pokedex.csv")
 
 # Plot random images from a list or dictionary of image paths
 # =====================================================================
-def plot_image_paths(image_paths: list, n: int = 6) -> None:
-    """Plot n random images from the list or dictionary of image paths
-
-    :param image_paths: A list or dictionary of image paths
-    :param n: The number of images to plot
+def plot_image_paths(image_paths: Union[List[str], Dict[str, str]], n: int = 6) -> None:
     """
+    Plot n random images from a list or dictionary of image paths.
 
+    Args:
+        image_paths (Union[List[str], Dict[str, str]]): A list or dictionary of image paths.
+        n (int): The number of images to plot. Defaults to 6.
+
+    Raises:
+        ValueError: If image_paths is neither a list nor a dictionary.
+    """
     plt.figure(figsize=(20, 3))
 
     if isinstance(image_paths, list):
@@ -61,16 +66,18 @@ def plot_image_paths(image_paths: list, n: int = 6) -> None:
 def plot_images_batch(
     dataset_tf: tf.data.Dataset, df: pd.DataFrame = poke_df, n: int = 6
 ) -> None:
-    """Plots a batch of images with their labels, if the time_steps are given plot them too
-
-    :param dataset_tf: The tensorflow dataset with the images and labels
-    :param n: The number of images to plot
-    :param df: The dataframe with the pokemon data
-    :return: None
     """
+    Plots a batch of images with their labels, if the time_steps are given plot them too.
 
+    Args:
+        dataset_tf (tf.data.Dataset): The tensorflow dataset with the images and labels.
+        n (int): The number of images to plot. Defaults to 6.
+        df (pd.DataFrame): The dataframe with the pokemon data.
+    """
     plt.figure(figsize=(20, 3))
-    for img_batch, label_batch in dataset_tf.shuffle(buffer_size=1000).take(1):
+
+    random_pokemon_dataset = dataset_tf.shuffle(buffer_size=len(dataset_tf)).take(1)
+    for img_batch, label_batch in random_pokemon_dataset:
         for i in range(n):
             plt.subplot(1, n, i + 1)
             plt.imshow(img_batch[i] * 0.5 + 0.5)  # [0, 1] instead of [-1, 1]
@@ -85,33 +92,29 @@ def plot_images_batch(
 # =====================================================================
 
 
-# plot noise levels
-# =====================================================================
 def plot_noise_levels(T: int, beta_start: float, beta_end: float, s: float) -> None:
-    """Plots the noise levels for linear and cosine beta schedules
-
-    :param T: The number of time_steps
-    :param beta_start: The starting value of beta
-    :param beta_end: The ending value of beta
-    :param s: The scale factor for the variance curve
     """
+    Plots the noise levels for linear and cosine beta schedules.
 
-    # Normalize the diffusion step by T for plotting
+    Args:
+        T (int): The number of time_steps.
+        beta_start (float): The starting value of beta.
+        beta_end (float): The ending value of beta.
+        s (float): The scale factor for the variance curve.
+    """
     normalized_steps = np.linspace(0, 1, T)  # diffusion step (t/T)
 
     # Variance scheduler for noise level
-    # =====================================================================
     beta_linear = DiffusionModel.beta_scheduler("linear", T, beta_start, beta_end, s)
     beta_cosine = DiffusionModel.beta_scheduler("cosine", T, beta_start, beta_end, s)
 
     alpha_linear = 1.0 - beta_linear
     alpha_cosine = 1.0 - beta_cosine
 
-    aplha_cumprod_linear = np.cumprod(alpha_linear)
-    aplha_cumprod_cosine = np.cumprod(alpha_cosine)
+    aplha_cumprod_linear = tf.math.cumprod(alpha_linear)
+    aplha_cumprod_cosine = tf.math.cumprod(alpha_cosine)
 
     # Plot each scheduler in subplots
-    # =====================================================================
     plt.figure(figsize=(10, 7))
 
     # Noise levels
@@ -124,8 +127,6 @@ def plot_noise_levels(T: int, beta_start: float, beta_end: float, s: float) -> N
     plt.legend()
 
 
-# # Plot the forward diffusion function in 10 different ascending time_steps for the same image
-# # =====================================================================
 def plot_forward_diffusion(
     X: tf.Tensor,
     scheduler: str,
@@ -135,35 +136,35 @@ def plot_forward_diffusion(
     beta_end: float,
     s: float,
 ) -> None:
-    """Plot the forward diffusion function in 10 different ascending time_steps for the same image
-
-    :param X: The image to diffuse
-    :param scheduler: The beta scheduler
-    :param n_timesteps: The number of time_steps to plot
-    :param T: The total number of time_steps
-    :param beta_start: The starting value of beta
-    :param beta_end: The ending value of beta
-    :param s: The scale factor for the variance curve
-
-    :return: The diffused image at a time_step t
     """
+    Plot the forward diffusion function in 10 different ascending time_steps for the same image.
 
-    n_timesteps = np.linspace(0, T - 1, timesteps, dtype=np.int32)  # Get the time_steps
+    Args:
+        X (tf.Tensor): The image to diffuse.
+        scheduler (str): The beta scheduler.
+        timesteps (int): The number of time_steps to plot.
+        T (int): The total number of time_steps.
+        beta_start (float): The starting value of beta.
+        beta_end (float): The ending value of beta.
+        s (float): The scale factor for the variance curve.
+
+    Returns:
+        None
+    """
     plt.figure(figsize=(20, 7))
+    n_timesteps = np.linspace(0, T - 1, timesteps, dtype=np.int32)
 
     for i, t in enumerate(n_timesteps):
         plt.subplot(1, len(n_timesteps), i + 1)
-        plt.imshow(
-            np.clip(
-                DiffusionModel.forward_diffusion(
-                    X, t, T, scheduler, beta_start, beta_end, s
-                )
-                * 0.5
-                + 0.5,
-                a_min=0.0,
-                a_max=1.0,
-            )
-        )  # [0, 1] instead of [-1, 1]
+
+        diffused_img_tensor = DiffusionModel.forward_diffusion(
+            X, t, T, scheduler, beta_start, beta_end, s
+        )
+        # [0, 1] instead of [-1, 1]:
+        clipped_img = np.clip(diffused_img_tensor * 0.5 + 0.5, a_min=0.0, a_max=1.0)
+
+        plt.imshow(clipped_img)
+
         if t == 0:
             plt.title(f"{scheduler}\nt:{t}")
         else:
